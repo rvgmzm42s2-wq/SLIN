3bca5edbb6b9b95fa638094f06e9caf14bdc25a5
rvgmzm42s2-wq <rvgmzm42s2@privaterelay.appleid.com>
2026-09-18 16:23:35 -0400

    Strengthen native context model

--- cognition/context_model.ps1
+++ cognition/context_model.ps1
@@ -1,3 +1,18 @@
 function New-SLINContext([hashtable]$Input) {
-  [pscustomobject]@{timestamp=(Get-Date).ToUniversalTime().ToString('o');facts=@($Input.Facts);goals=@($Input.Goals);observations=@($Input.Observations);session=$Input.Session}
+  $facts=@($Input.Facts)
+  $goals=@($Input.Goals)
+  $observations=@($Input.Observations)
+  $session=$Input.Session
+
+  [pscustomobject]@{
+    timestamp=(Get-Date).ToUniversalTime().ToString('o')
+    facts=$facts
+    goals=$goals
+    observations=$observations
+    session=$session
+    factCount=$facts.Count
+    goalCount=$goals.Count
+    observationCount=$observations.Count
+    hasPriorContext=($facts.Count -gt 0 -or $observations.Count -gt 0)
+  }
 }
